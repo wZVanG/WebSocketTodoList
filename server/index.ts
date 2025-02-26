d10e64294a2249b23f0317d0e1ac4eec977345f2
Walter Chapilliquen <wz.vang@gmail.com>
2025-02-26 13:34:43 -0500

    vercel fix

--- server/index.ts
+++ server/index.ts
@@ -56,14 +56,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
-  const port = 5000;
-  server.listen({
-    port,
-    host: "127.0.0.1",
-    //reusePort: true,
-  }, () => {
+  const port = process.env.PORT || 5000;
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
